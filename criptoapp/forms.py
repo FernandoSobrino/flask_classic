089b1077706c692cabeb060b60d318ecc45e30dd
Fernando Sobrino <sobrino_fernando@yahoo.es>
2022-11-19 13:07:55 +0100

    feat: incluido archivo formulario

---
+++ criptoapp/forms.py
@@ -0,0 +1,18 @@
+from datetime import date, datetime
+from flask_wtf import FlaskForm
+from wtforms import DateField, StringField, SelectField, SubmitField, TimeField, IntegerField
+from wtforms.validators import DataRequired
+
+class CryptoForm(FlaskForm):
+
+    fecha = DateField('Fecha')
+    hora = TimeField('Hora')
+    moneda1 = SelectField('De: ', choices=[("EUR","Euro"),("ETH","Ethereum"),("BNB","Binance Coin")
+    ,("BTC","Bitcoin")], validators=[DataRequired(message="Selecciona moneda inicial")])
+    moneda2 = SelectField('A: ', choices=[("EUR", "Euro"), ("ETH", "Ethereum"), ("BNB", "Binance Coin"), (
+        "BTC", "Bitcoin")], validators=[DataRequired(message="Selecciona moneda inicial")])
+    cantidad = IntegerField('Cantidad', validators=[DataRequired(message="Debes indicar una cantidad")])
+    consultarapi = SubmitField("Consultar API")
+    #borrar = SubmitField("Borrar")
+    #enviar = SubmitField("Guardar")
+    textoapi = StringField()
