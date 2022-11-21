e6cc89f2a7b907ab99bcd9d4f067522b005829f3
Fernando Sobrino <sobrino_fernando@yahoo.es>
2022-11-21 22:01:59 +0100

    refactor: monedas de forms constante global

--- criptoapp/forms.py
+++ criptoapp/forms.py
@@ -3,14 +3,14 @@ from flask_wtf import FlaskForm
 from wtforms import DateField, StringField, SelectField, SubmitField, TimeField, IntegerField
 from wtforms.validators import DataRequired
 
+from .settings import MONEDAS
+
 class CryptoForm(FlaskForm):
 
     fecha = DateField('Fecha')
     hora = TimeField('Hora')
-    moneda_origen = SelectField('De: ', choices=[("EUR","Euro"),("ETH","Ethereum"),("BNB","Binance Coin")
-    ,("BTC","Bitcoin")], validators=[DataRequired(message="Selecciona moneda inicial")])
-    moneda_destino = SelectField('A: ', choices=[("EUR", "Euro"), ("ETH", "Ethereum"), ("BNB", "Binance Coin"), (
-        "BTC", "Bitcoin")], validators=[DataRequired(message="Selecciona moneda inicial")])
+    moneda_origen = SelectField('De: ', choices=MONEDAS, validators=[DataRequired(message="Selecciona moneda inicial")])
+    moneda_destino = SelectField('A: ', choices=MONEDAS, validators=[DataRequired(message="Selecciona moneda inicial")])
     cantidad = IntegerField('Cantidad', validators=[DataRequired(message="Debes indicar una cantidad")])
     consultarapi = SubmitField("Consultar API")
     borrar = SubmitField("Borrar")
